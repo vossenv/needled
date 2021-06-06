6164f6529c0db7485f5b85d0556d7f67a7fc0681
vossen-adobe <per38285@adobe.com>
2021-06-06 18:56:04 -0500

    players impressum

--- js/tp_page.js
+++ js/tp_page.js
@@ -14,13 +14,23 @@ document.addEventListener("DOMContentLoaded", function (e) {
     observer.observe(document.getElementById('intersect'));
 
     var x = document.getElementsByClassName("flip-card");
-     for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++) {
         const r = x[i];
-        console.log(r)
+        // console.log(r)
         r.addEventListener("click", function () {
             r.toggleAttribute('flipped');
         });
     }
 
+    var y = document.getElementsByClassName("dept-title");
+    for (i = 0; i < y.length; i++) {
+        const r = y[i];
+        const dn = r.getAttribute("dname");
+        console.log(dn)
+        r.addEventListener("click", function () {
+            document.getElementById(dn).toggleAttribute('xhidden');
+            document.getElementById(dn + "-arrow").toggleAttribute('xhidden');
+        });
+    }
 
 });
--- template/front_page.php
+++ template/front_page.php
@@ -52,13 +52,13 @@ get_header(); ?>
             <div id="verein" class="trans_row">
                 <span class="section-title">TP.tz Verein</span><br/>
                 <hr>
-                <br/>
                 <?php
                 $depts_players = players_by_dept();
                 foreach ($depts_players as &$v) {
-
-                    echo '<div classs="department"><span class="dept-title">' . $v['department'] . '<br/></span>';
-
+                    $dn = "d-" . $v['department'];
+                    echo '<div class="department"><div dname="'.$dn.'" class="dept-title" >' . $v['department'] .
+                        '<span class="darrow" id="'.$dn.'-arrow">▼</span><br/></div>';
+                    echo '<div xhidden class="dept-div" id="' . $dn . '">';
                     foreach ($v['players'] as &$p) {
 
                         $img_f = $p['Front Picture'] == NULL ?
@@ -93,25 +93,25 @@ get_header(); ?>
                         echo "</div>";
                     }
 
-                    echo "</div>";
+                    echo "</div></div>";
 
                 }
                 ?>
 
             </div>
 
-            <div id="paintball" class="trans_row section-title">
+            <div id="paintball" class="trans_row">
                 <span class="section-title">Paintball</span>
                 <hr>
             </div>
-            <div id="social" class="trans_row section-title">
+            <div id="soziales" class="trans_row">
                 <span class="section-title">Social</span>
                 <hr>
 
                 <div id="fb_social" class="social_col">
                     <span class="social-title">Facebook</span>
                     <div class="fb-page" data-href="https://www.facebook.com/TriggeredPaintz"
-                         data-tabs="timeline" data-width="" data-height="664" data-small-header="false"
+                         data-tabs="timeline" data-width="" data-height="665" data-small-header="false"
                          data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
                 </div>
 
@@ -120,15 +120,44 @@ get_header(); ?>
                     <?php echo do_shortcode('[wdi_feed id="1"]'); ?>
                 </div>
             </div>
-            <div id="events" class="trans_row section-title">
+            <div id="events" class="trans_row">
                 <span class="section-title">Events</span>
                 <hr>
             </div>
-            <div id="contact" class="trans_row section-title">
-                <span class="section-title">Contact</span>
+            <div id="kontakt" class="trans_row">
+                <span class="section-title">Kontakt</span>
                 <hr>
+                <br/>
                 <p>Haben wir dein Interesse an unserem Team geweckt? Wir freuen uns auf deine Nachricht!</p>
                 <?php echo do_shortcode('[wpforms id="281"]'); ?>
+            </div>
+            <div id="impressum" class="trans_row">
+                <span class="section-title">Impressum</span>
+                <hr>
+                <div class="impressum-text">
+                    <table id="impressum-table">
+                        <tr>
+                            <td class='field_name'>Rechtsform:</td>
+                            <td>Eingetragener Verein</td>
+                        </tr>
+                        <tr>
+                            <td class='field_name'>Adresse:</td>
+                            <td>TriggeredPain.tz e.V</td>
+                        </tr>
+                        <tr>
+                            <td class='field_name'>Telefon:</td>
+                            <td>12345868</td>
+                        </tr>
+                        <tr>
+                            <td class='field_name'>E-Mail:</td>
+                            <td><a href="mailto:someone@yoursite.com">vorstand@triggeredpaintz.com</a></td>
+                        </tr>
+                        <tr>
+                            <td class='field_name'>Register:</td>
+                            <td>E127491234798</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
 
             <?php get_template_part('template-parts/content', 'single'); ?>
